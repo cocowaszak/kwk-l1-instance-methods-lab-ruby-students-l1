e767ffb3c584033322ee07cd86378a1a6f1d91bf
cocowaszak <colettewaszak@gmail.com>
2018-06-07 21:45:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# dog.rb
+class Dog 
+  def initialize(bark, sit)
+    @bark = bark
+    @sit = sit
+  end
+   def bark 
+    @bark 
+  end 
+  def bark=(bark)
+    @bark = bark 
+  end 
+  def sit 
+    @sit 
+  end 
+  def sit=(sit)
+    @sit = sit 
+  end 
+end 
+dog1 = Dog.new("Woof!", "The Dog is sitting")
+
